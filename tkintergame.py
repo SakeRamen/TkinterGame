7b78f97fd7dfa6e1f7e9e22dbbb182a35be40ebb
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-19 14:53:02 +0300

    add randomPoz

--- tkintergame.py
+++ tkintergame.py
@@ -1,1 +1,14 @@
-print('Hi,world!')
+import tkinter as tk
+import random
+
+def randomPoz(top):
+    return random.randint(1,top-1)*step
+    
+master = tk.Tk()
+step = 60
+N_X = 10
+N_Y = 10
+canvas = tk.Canvas(master,bg='#BACE72',height = step*N_X,width=step*N_Y)
+
+player_pos = (randomPoz(N_X), randomPoz(N_Y))
+exit_pos   = (randomPoz(N_X), randomPoz(N_Y))
