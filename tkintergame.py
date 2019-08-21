107acb83684a3a7a2640f10bb396164cc4983484
SakeRamen <53336557+SakeRamen@users.noreply.github.com>
2019-08-21 14:47:30 +0300

    added function KeyPress and Endgame

--- tkintergame.py
+++ tkintergame.py
@@ -9,10 +9,37 @@ class Player:
         this.color = color
 
     def draw(this):
-        canvas.create_oval((this.x,this.y),(this.x+step,this.y+step),fill=this.color)
+        this.player = canvas.create_oval((this.x,this.y),(this.x+step,this.y+step),fill=this.color)
         
     def randomPoz(this, top):
         return random.randint(1,top-1)*step
+
+    def repaint(this, x, y):
+        canvas.move(this.player, x, y)
+
+    def checkPos(this, other):
+        return ((this.x == other.x) and (this.y == other.y))
+
+def keyPress(event):
+    if event.keycode == 87:
+        player.y -= step
+        player.repaint(0, -step)
+    elif event.keycode == 83:
+        player.y += step
+        player.repaint(0, step)
+    elif event.keycode == 65:
+        player.x -= step
+        player.repaint(-step, 0)
+    elif event.keycode == 68:
+        player.x += step
+        player.repaint(step, 0)
+    endGame()
+
+def endGame():
+    if player.checkPos(exit_g):
+        print('Game over')
+        print('You won!')
+
     
 master = tk.Tk()
 step = 60
@@ -26,4 +53,5 @@ exit_g = Player('#F7F8A2')
 exit_g.draw();
 
 canvas.pack()
+master.bind('<KeyPress>', keyPress)
 master.mainloop()
